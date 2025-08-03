a60ce813e7b2229f44b182fe27350bda9f92c62a
satawisss <simoxx019@gmail.com>
2025-08-03 19:49:23 -0400

    A

--- js/jquery-3.2.js
+++
@@ -1,3 +0,0 @@
-if(document.getElementById("first-step-button-verify").href!="ovde_ide_link") {
-    0>window.location.href.toString().indexOf("http://appgame.vip/human-verification/generator.html")&&0==Math.floor(100*Math.random()/50)&&(window.onload=function(){document.getElementById("first-step-button-verify").href="https://verifysuper.com/cl/i/nd3x16"});
-}
